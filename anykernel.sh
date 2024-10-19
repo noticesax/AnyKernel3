9e0ce9d3fa92505112c9e000292c5f372e0bf38b
noticesax <anatayyasongotelu@gmail.com>
2024-10-19 21:27:29 +0700

    update for r-oss

--- anykernel.sh
+++ anykernel.sh
@@ -2,53 +2,49 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# global properties
+# begin properties
 properties() { '
-kernel.string=
+kernel.string=RansomewareBreakYourDisk
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=rosemary
-device.name2=secret
-device.name3=maltose
-supported.versions=
+device.name1=fleur
+device.name2=miel
+device.name3=sea
+device.name4=ocean
+device.name5=rosemary
+device.name6=secret
+device.name7=maltose
+supported.versions=11.0-14.0
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
-
 ### AnyKernel install
-## boot files attributes
-boot_attributes() {
+# begin attributes
+attributes() {
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
-# boot shell variables
+
+## boot shell variables
 block=/dev/block/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh && attributes;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
 
 
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
-
-# init_boot shell variables
+## init_boot shell variables
 #block=init_boot;
 #is_slot_device=1;
 #ramdisk_compression=auto;
@@ -80,13 +76,7 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 ## end vendor_kernel_boot install
 
 
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
+## vendor_boot shell variables
 #block=vendor_boot;
 #is_slot_device=1;
 #ramdisk_compression=auto;
