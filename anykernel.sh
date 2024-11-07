1998dcd16771faad951d59b6831d2a40906ee6eb
noticesax <anatayyasongotelu@gmail.com>
2024-11-07 18:01:43 +0000

    ak3: update

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=RansomewareBreakYourDisk
+kernel.string=Magneto Kernel Rosemary by noticesa@merlinx
 do.devicecheck=1
 do.modules=0
 do.systemless=1
