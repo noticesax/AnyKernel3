77d6fca782a4bbeced0396a476871d20daf60b37
noticesax <anatayyasongotelu@gmail.com>
2024-11-09 06:51:35 +0000

    update ak3.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Magneto Kernel by noticesa@merlinx
+kernel.string=Alchemy Kernel by noticesa@merlinx
 do.devicecheck=0
 do.modules=0
 do.systemless=1
